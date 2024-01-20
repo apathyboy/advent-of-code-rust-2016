2655b83820f503b140df336ce6de2d9cf0cefe1e
eb <eb@eb2.co>
2024-01-20 12:59:24 -0700

    Refactor code to use PrototypeComputer struct

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,85 +1,27 @@
-use std::collections::HashMap;
+use advent_of_code::PrototypeComputer;
 
 advent_of_code::solution!(12);
 
-fn run_program(registers: &mut HashMap<&str, i32>, program: &[&str]) {
-    let mut ip = 0;
-
-    loop {
-        if ip >= program.len() {
-            break;
-        }
-
-        let instruction = program[ip];
-        let (name, rest) = instruction.split_once(' ').unwrap();
-
-        match name {
-            "cpy" => {
-                let (source, dest) = rest.split_once(' ').unwrap();
-
-                let source_val = if registers.contains_key(source) {
-                    *registers.get(source).unwrap()
-                } else {
-                    source.parse::<i32>().unwrap()
-                };
-
-                *registers.get_mut(dest).unwrap() = source_val;
-
-                ip += 1;
-            }
-            "inc" => {
-                *registers.get_mut(rest).unwrap() += 1;
-
-                ip += 1;
-            }
-            "dec" => {
-                *registers.get_mut(rest).unwrap() -= 1;
-
-                ip += 1;
-            }
-            "jnz" => {
-                let (source, dest) = rest.split_once(' ').unwrap();
-
-                let x_val = if registers.contains_key(source) {
-                    *registers.get(source).unwrap()
-                } else {
-                    source.parse::<i32>().unwrap()
-                };
-
-                if x_val != 0 {
-                    let skip = dest.parse::<i32>().unwrap();
-
-                    ip = if skip < 0 {
-                        let skip = (-skip) as usize;
-                        ip.checked_sub(skip).unwrap()
-                    } else {
-                        ip.checked_add(skip as usize).unwrap()
-                    };
-                } else {
-                    ip += 1;
-                }
-            }
-            _ => panic!("Invalid instruction {name}"),
-        };
-    }
-}
-
 pub fn part_one(input: &str) -> Option<i32> {
-    let mut registers: HashMap<&str, i32> = HashMap::from([("a", 0), ("b", 0), ("c", 0), ("d", 0)]);
     let program = input.lines().collect::<Vec<_>>();
 
-    run_program(&mut registers, &program);
+    let mut computer = PrototypeComputer::new();
 
-    registers.get(&"a").copied()
+    computer.run_program(&program);
+
+    computer.get_register('a')
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    let mut registers: HashMap<&str, i32> = HashMap::from([("a", 0), ("b", 0), ("c", 1), ("d", 0)]);
     let program = input.lines().collect::<Vec<_>>();
 
-    run_program(&mut registers, &program);
+    let mut computer = PrototypeComputer::new();
 
-    registers.get(&"a").copied()
+    computer.set_register('c', 1);
+
+    computer.run_program(&program);
+
+    computer.get_register('a')
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
@@ -9,4 +11,107 @@ pub fn make_secret(input: &str, suffix: u32) -> String {
 pub fn make_hash(input: &str) -> String {
     let digest = md5::compute(input);
     format!("{:x}", digest)
+}
+
+pub struct PrototypeComputer {
+    registers: HashMap<char, i32>,
+    instruction_pointer: usize,
+}
+
+impl PrototypeComputer {
+    pub fn new() -> Self {
+        Self {
+            registers: HashMap::from([('a', 0), ('b', 0), ('c', 0), ('d', 0)]),
+            instruction_pointer: 0,
+        }
+    }
+
+    pub fn get_register(&self, register: char) -> Option<i32> {
+        self.registers.get(&register).copied()
+    }
+
+    pub fn set_register(&mut self, register: char, val: i32) {
+        *self.registers.get_mut(&register).unwrap() = val;
+    }
+
+    fn cpy(&mut self, command: &str) {
+        let (source, dest) = command.split_once(' ').unwrap();
+        let source_register = source.chars().next().unwrap();
+        let dest_register = dest.chars().next().unwrap();
+
+        let source_val = if self.registers.contains_key(&source_register) {
+            *self.registers.get(&source_register).unwrap()
+        } else {
+            source.parse::<i32>().unwrap()
+        };
+
+        *self.registers.get_mut(&dest_register).unwrap() = source_val;
+
+        self.instruction_pointer += 1;
+    }
+
+    fn inc(&mut self, command: &str) {
+        let target_register = command.chars().next().unwrap();
+        *self.registers.get_mut(&target_register).unwrap() += 1;
+
+        self.instruction_pointer += 1;
+    }
+
+    fn dec(&mut self, command: &str) {
+        let target_register = command.chars().next().unwrap();
+        *self.registers.get_mut(&target_register).unwrap() -= 1;
+
+        self.instruction_pointer += 1;
+    }
+
+    fn jnz(&mut self, command: &str) {
+        let (source, dest) = command.split_once(' ').unwrap();
+        let source_register = source.chars().next().unwrap();
+
+        let x_val = if self.registers.contains_key(&source_register) {
+            *self.registers.get(&source_register).unwrap()
+        } else {
+            source.parse::<i32>().unwrap()
+        };
+
+        if x_val != 0 {
+            let skip = dest.parse::<i32>().unwrap();
+
+            self.instruction_pointer = if skip < 0 {
+                let skip = (-skip) as usize;
+                self.instruction_pointer.checked_sub(skip).unwrap()
+            } else {
+                self.instruction_pointer.checked_add(skip as usize).unwrap()
+            };
+        } else {
+            self.instruction_pointer += 1;
+        }
+    }
+
+    pub fn run_program(&mut self, program: &[&str]) {
+        loop {
+            if self.instruction_pointer >= program.len() {
+                break;
+            }
+
+            let instruction = program[self.instruction_pointer];
+            let (name, rest) = instruction.split_once(' ').unwrap();
+
+            match name {
+                "cpy" => {
+                    self.cpy(rest);
+                }
+                "inc" => {
+                    self.inc(rest);
+                }
+                "dec" => {
+                    self.dec(rest);
+                }
+                "jnz" => {
+                    self.jnz(rest);
+                }
+                _ => panic!("Invalid instruction {name}"),
+            };
+        }
+    }
 }
