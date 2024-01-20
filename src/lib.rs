4033925e51a1a2b296fa63be7c1c466eefe90eea
eb <eb@eb2.co>
2024-01-20 14:20:00 -0700

    Refactor PrototypeComputer instructions and add program parsing

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,20 @@ pub fn make_hash(input: &str) -> String {
     format!("{:x}", digest)
 }
 
+struct PrototypeComputerInstruction {
+    name: String,
+    arguments: Vec<String>,
+}
+
+impl PrototypeComputerInstruction {
+    fn new(name: &str, arguments: Vec<String>) -> Self {
+        Self {
+            name: name.to_string(),
+            arguments,
+        }
+    }
+}
+
 pub struct PrototypeComputer {
     registers: HashMap<char, i32>,
     instruction_pointer: usize,
@@ -40,8 +54,7 @@ impl PrototypeComputer {
         *self.registers.get_mut(&register).unwrap() = val;
     }
 
-    fn cpy(&mut self, command: &str) {
-        let (source, dest) = command.split_once(' ').unwrap();
+    fn cpy(&mut self, source: &str, dest: &str) {
         let source_register = source.chars().next().unwrap();
         let dest_register = dest.chars().next().unwrap();
 
@@ -56,22 +69,21 @@ impl PrototypeComputer {
         self.instruction_pointer += 1;
     }
 
-    fn inc(&mut self, command: &str) {
-        let target_register = command.chars().next().unwrap();
+    fn inc(&mut self, target: &str) {
+        let target_register = target.chars().next().unwrap();
         *self.registers.get_mut(&target_register).unwrap() += 1;
 
         self.instruction_pointer += 1;
     }
 
-    fn dec(&mut self, command: &str) {
-        let target_register = command.chars().next().unwrap();
+    fn dec(&mut self, target: &str) {
+        let target_register = target.chars().next().unwrap();
         *self.registers.get_mut(&target_register).unwrap() -= 1;
 
         self.instruction_pointer += 1;
     }
 
-    fn jnz(&mut self, command: &str) {
-        let (source, dest) = command.split_once(' ').unwrap();
+    fn jnz(&mut self, source: &str, dest: &str) {
         let source_register = source.chars().next().unwrap();
 
         let x_val = if self.registers.contains_key(&source_register) {
@@ -94,29 +106,42 @@ impl PrototypeComputer {
         }
     }
 
+    fn parse_program(&self, program: &[&str]) -> Vec<PrototypeComputerInstruction> {
+        program
+            .iter()
+            .filter_map(|instruction| {
+                let (name, arguments) = instruction.split_once(' ')?;
+                let arguments = arguments.split(' ').map(|s| s.to_string()).collect();
+
+                Some(PrototypeComputerInstruction::new(name, arguments))
+            })
+            .collect::<Vec<_>>()
+    }
+
     pub fn run_program(&mut self, program: &[&str]) {
+        let program = self.parse_program(program);
+
         loop {
             if self.instruction_pointer >= program.len() {
                 break;
             }
 
-            let instruction = program[self.instruction_pointer];
-            let (name, rest) = instruction.split_once(' ').unwrap();
+            let instruction = &program[self.instruction_pointer];
 
-            match name {
+            match instruction.name.as_str() {
                 "cpy" => {
-                    self.cpy(rest);
+                    self.cpy(&instruction.arguments[0], &instruction.arguments[1]);
                 }
                 "inc" => {
-                    self.inc(rest);
+                    self.inc(&instruction.arguments[0]);
                 }
                 "dec" => {
-                    self.dec(rest);
+                    self.dec(&instruction.arguments[0]);
                 }
                 "jnz" => {
-                    self.jnz(rest);
+                    self.jnz(&instruction.arguments[0], &instruction.arguments[1]);
                 }
-                _ => panic!("Invalid instruction {name}"),
+                _ => panic!("Invalid instruction {}", instruction.name),
             };
         }
     }
